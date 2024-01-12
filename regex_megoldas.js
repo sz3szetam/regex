2a6e65bedc925ea4487366d350f4e1e076168be8
Szécsi Tamás Dávid <144216869+sz3szetam@users.noreply.github.com>
2024-01-12 19:37:32 +0100

    Add files via upload

---
+++ regex_megoldas.js
@@ -0,0 +1,7 @@
+// Regex megoldasok
+var Regex_Pattern = 'hackerrank'; // 1. megoldas
+var Regex_Pattern = /^(\S{3}\.)(\S{3}\.){2}(\S{3})$/g // 2. megoldas
+var Regex_Pattern = /\d{2}\D{1}\d{2}\D{1}\d{4}/; // 3. megoldas
+var Regex_Pattern = /\S{2}\s\S{2}\s\S{2}/ // 4. megoldas
+var Regex_Pattern = /\w{3}\W\w{10}\W\w{3}/ // 5. megoldas
+var Regex_Pattern = /^\d\w{4}\.$/; // 6. megoldas
